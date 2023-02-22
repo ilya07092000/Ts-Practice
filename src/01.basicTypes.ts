a3d64ac4489054e31f7be528427f63297c7be84c
ilya07092000 <ischenko2012@gmail.com>
2023-02-22 11:05:48 +0200

    feat: basic types

--- src/01.basicTypes.ts
+++ src/01.basicTypes.ts
@@ -1,1 +1,14 @@
+const number: number = 123;
+const string: string = '123';
+const boolean: boolean = true;
+
+const num1: number = 1;
+const num2: number = 2;
+
+function add(n1: number, n2: number): number {
+	return n1 + n2;
+}
+
+console.log('add()', add(num1, num2));
+
 export {};
