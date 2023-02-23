8b7734b376053f7aa17b38b6ce9ccd0f202382cb
ilya07092000 <ischenko2012@gmail.com>
2023-02-23 10:20:18 +0200

    feat: basic types

--- src/01.basicTypes.ts
+++ src/01.basicTypes.ts
@@ -1,3 +1,5 @@
+// NUMBERS & STRINGS & BOOLEANS
+
 const number: number = 123;
 const string: string = '123';
 const boolean: boolean = true;
@@ -5,10 +7,25 @@ const boolean: boolean = true;
 const num1: number = 1;
 const num2: number = 2;
 
-function add(n1: number, n2: number): number {
-	return n1 + n2;
+let numType = 123; // automatically detects type (number)
+numType = '123'; // automatically detects type (number)
+
+const printResult: boolean = true;
+const resultPhrase: string = 'Result is';
+
+function add(
+	n1: number,
+	n2: number,
+	showResult: boolean,
+	phrase: string
+): string {
+	if (showResult) {
+		return `${phrase} ${n1 + n2}`;
+	}
+
+	return 'no result';
 }
 
-console.log('add()', add(num1, num2));
+console.log('add()', add(num1, num2, printResult, resultPhrase));
 
 export {};
