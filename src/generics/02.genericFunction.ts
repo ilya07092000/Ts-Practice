a3eea7b8956e5b9f671410055962a0150cce037f
ilya07092000 <ischenko2012@gmail.com>
2023-03-06 08:25:30 +0200

    generic constraints

--- src/generics/02.genericFunction.ts
+++ src/generics/02.genericFunction.ts
@@ -11,4 +11,13 @@ const mergedObjs2 = merge<{ name: string }, { age: number }>(
 );
 mergedObjs2.age;
 
+// Type constraints
+
+function mergeObjects<T extends object, U extends object>(objA: T, objB: U) {
+	return Object.assign({}, objA, objB);
+}
+
+const mergedObjs3 = mergeObjects({ name: 'Ilya' }, { age: 22 });
+mergedObjs3.name;
+
 export {};
